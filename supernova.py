a1583cbcb2999457fab6fa49b277962b81d87a1b
Pugachev Dmitry <pugachevdmitry67@gmail.com>
2015-10-14 22:37:37 +0300

    change test supernova (reverted from commit d17a3ffb69218975b62f5e9d9aa4beab9e39cfab)

--- supernova.py
+++ supernova.py
@@ -1,24 +1,8 @@
-from superSpectr import *
-from firstorder import *
-import math
-import numpy
-
-class SupernovaNeutrino:
-    def __init__(self, eNu):
-        s = SuperSpectr(eNu)
-        self.enNu = s.shootEnu(100000)
-        self.firstOrder = FirstOrder(10)
-
-    def shootPos(self):
-        posAngle = []
-        posEn    = []
-        kinN       = []
-        for eNu in self.enNu:
-            self.firstOrder = FirstOrder(eNu)
-            cos = self.firstOrder.shootPosAngle(1)
-            pE  = self.firstOrder.enPos(cos)
-            kN  = self.firstOrder.kinNeutron(cos)
-            posAngle.append(cos)
-            posEn.append(pE)
-            kinN.append(kN)
-        return numpy.asarray(posAngle), numpy.asarray(posEn), numpy.asarray(kinN)
+class Supernova:
+    def __init__(self, ev):
+        self.ev = ev                #neutrino energy
+        self.e0  = self.ev - delt   #positron energy in null order
+        self.ve0 = sqrt(self.e0*self.e0 - me*me)/self.e0  #positron velocity in null order
+        self.p_probability = []
+        self.p_energyBin   = []
+        self.auxiliaryFunc() #init self.p_probability & self.p_energyBin
